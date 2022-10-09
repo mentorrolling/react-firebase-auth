f5e204d3961090765e764c30ab5180966f812166
Pablo Marino <pmarino2013@gmail.com>
2022-10-09 17:17:12 -0300

    Configuro variables de entorno con datos de la conexion a firebase

--- src/firbase.js
+++ src/firbase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMsgLeEBbuVYLhtcypPKlIxr45fX3oCcE",
-  authDomain: "react-authentication-sitenso.firebaseapp.com",
-  projectId: "react-authentication-sitenso",
-  storageBucket: "react-authentication-sitenso.appspot.com",
-  messagingSenderId: "38989667194",
-  appId: "1:38989667194:web:bc4ec682f8ec9ef7f7d4c3",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
